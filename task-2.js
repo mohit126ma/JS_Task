873679920ddff8e52a5ae96cc475a0bd615a76b7
mohit126ma <mohit126ma@gmail.com>
2026-08-31 17:27:22 +0530

    Add files via upload
    task updated

---
+++ task-2.js
@@ -0,0 +1,231 @@
+// //Task-1
+// var studentName = "Mohit Abbigeri";
+// let studentAge = 26;
+// const collegeName = "KLE Independent College";
+// console.log(studentName);
+// console.log(studentAge);
+// console.log(collegeName);
+
+// // Change var
+// studentName = "Abbigeri";
+// console.log(studentName);
+// // Change let
+// studentAge = 26;
+// console.log(studentAge);
+// var studentName = "Koustubh";
+// console.log(studentName);
+
+
+// //task-2
+
+// var name = prompt("Enter your name:");
+// var age = prompt("Enter your age:");
+// var city = prompt("Enter your city:");
+
+// console.log("Name:", name);
+// console.log("Age:", age);
+// console.log("City:", city);
+
+// //task-3
+// var name = prompt("Enter your name:");
+
+// alert("Welcome " + name + "!");//Welcome Mohit
+
+// //task-4
+// var birthYear = prompt("Enter your birth year:");
+
+// var currentYear = 2026;
+// var age = currentYear - birthYear;
+
+// console.log("Birth Year:", birthYear);//Birth Year: 2000
+// console.log("Age:", age);//Age: 26
+
+// //task-5
+// var text = "Hello";
+// var number = 100;
+// var decimal = 25.5;
+// var trueValue = true;
+// var falseValue = false;
+// var undefinedValue;
+// var nullValue = null;
+
+// console.log(typeof text); //string
+// console.log(typeof number);//number
+// console.log(typeof decimal);//number
+// console.log(typeof trueValue);//boolean
+// console.log(typeof falseValue);//boolean
+// console.log(typeof undefinedValue);//undefined
+// console.log(typeof nullValue);//object
+
+// //task-6
+// var student = {
+//     name: "Mohit Abbigeri",
+//     age: 26,
+//     city: "Pune",
+//     qualification: "Civil",
+//     isStudent: true
+// };
+
+// console.log(student);//{ name: 'Mohit Abbigeri', age: 26, city: 'Pune', qualification: 'Civil', isStudent: true }
+// console.log(student.name);//Mohit Abbigeri
+// console.log(student.age);//26
+// console.log(student.qualification);//Civil
+// console.log(student.isStudent);//true
+
+// //task-7
+// var fruits = [
+//     "Apple",
+//     "Mango",
+//     "Orange",
+//     "Banana",
+//     "Grapes",
+//     "Papaya"
+// ];
+// console.log("First fruit:", fruits[0]);//First fruit: Apple
+// console.log("Second fruit:", fruits[1]);//Second fruit: Mango
+// console.log("Last fruit:", fruits[fruits.length - 1]);//Last fruit: Papaya
+// console.log("Total number of fruits:", fruits.length);//Total number of fruits: 6
+
+// //task-8
+// let a = 20;
+// let b = 5;
+
+// console.log("Addition:", a + b);//Addition: 25
+// console.log("Subtraction:", a - b);//Subtraction: 15
+// console.log("Multiplication:", a * b);//Multiplication: 100
+// console.log("Division:", a / b);//Division: 4
+// console.log("Modulus:", a % b);//Modulus: 0
+// console.log("Exponentiation:", a ** b);//Exponentiation: 100000
+
+// //task-9
+// let shirt = 999;
+// let pant = 1499;
+// let shoes = 1999;
+
+// let total = shirt + pant + shoes;
+
+// console.log("Total =", total);//Total = 4497
+
+// //task-10
+// let tamil = 80;
+// let english = 75;
+// let maths = 90;
+
+// let total = tamil + english + maths;
+// let average = total / 3;
+
+// console.log("Total marks:", total);//Total marks: 245
+// console.log("Average marks:", average);//Average marks: 81.66666666666667
+
+// //task-11
+// let a = 10;
+// let b = a++;
+// console.log(a);//11
+// console.log(b);//10
+
+// //task-12
+// let a = 10;
+// let b = ++a;
+// console.log(a);//11
+// console.log(b);//11
+
+// //task-13
+// let a = 20;
+// let b = a--;
+// console.log(a);//19
+// console.log(b);//20
+
+// //task-14
+// let a = 20;
+// let b = --a;
+// console.log(a);//19
+// console.log(b);//19
+
+// //task-15
+// let a = 5;
+// let b = a++;
+// let c = ++a;
+// let d = b--;
+
+// console.log(a);//7
+// console.log(b);//5
+// console.log(c);//7
+// console.log(d);//5
+
+// //task-16
+// let num = 10;
+// num += 5;
+// console.log(num);//15
+
+// let num = 10;
+// num -= 5;
+// console.log(num);//5
+
+// let num = 10;
+// num *= 5;
+// console.log(num);//50
+
+// let num = 10;
+// num /= 5;
+// console.log(num);//2
+
+// let num = 10;
+// num %= 3;
+// console.log(num);//1
+
+// let num = 10;
+// num **= 2;
+// console.log(num);//100
+
+// //task-17
+// var name = "Mohit Abbigeri";
+// var age = 26;
+// var city = "Pune";
+// var college = "KLE Independent College";
+// var subjects = [
+//     "Kannada",
+//     "English",
+//     "Maths",
+//     "Science",
+//     "Computer"
+// ];
+// var student = {
+//     name: name,
+//     age: age,
+//     city: city,
+//     subjects: subjects,
+//     isStudent: true
+// };
+
+// console.log("Student name:", student.name);//Mohit Abbigeri
+// console.log("Student age:", student.age);//26
+// console.log("City:", student.city);//Pune
+// console.log("First subject:", student.subjects[0]);//Kannada
+// console.log("Last subject:", student.subjects[student.subjects.length - 1]);//Computer
+// console.log("Total subjects:", student.subjects.length);//5
+// console.log("Complete object:", student);//
+
+
+// //Final Challenge — User + Calculator
+
+// var firstNumber = prompt("Enter first number:");
+// var secondNumber = prompt("Enter second number:");
+
+// var addition = firstNumber + secondNumber;
+// var subtraction = firstNumber - secondNumber;
+// var multiplication = firstNumber * secondNumber;
+// var division = firstNumber / secondNumber;
+// var modulus = firstNumber % secondNumber;
+// var power = firstNumber ** secondNumber;
+
+// console.log("Addition:", addition);//
+// console.log("Subtraction:", subtraction);
+// console.log("Multiplication:", multiplication);
+// console.log("Division:", division);
+// console.log("Modulus:", modulus);
+// console.log("Power:", power);
+
+
+
+
+
